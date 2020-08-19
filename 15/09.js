21299e9503c4947850c069cf7ce7ee4d660396ef
ygjzs <1793523411@qq.com>
2020-08-19 22:20:32 +0800

    15.7

--- 15/09.js
+++ 15/09.js
@@ -10,7 +10,7 @@ function readLinesSync(file){
     }
 }
 
-//return必须返回一个对象，Generator规格决定
+//return必须返回一个对象，Generator规格决定，throw配合Generator使用，一般的遍历器对象用不到这个方法
 
 for(let line of readLinesSync(fileName)){
     console.log(line)
